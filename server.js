2892047e5189c87b3211cd4a6aaf6a11c6ceb25a
gzarbameli <105161189+gzarbameli@users.noreply.github.com>
2022-07-16 16:17:59 +0200

    Update server.js

--- server.js
+++ server.js
@@ -68,7 +68,7 @@ function fibo(n) {
   if (n === 1) {
     return [0, 1];
   } else {
-    var arr = fib(n - 1);
+    var arr = fibo(n - 1);
     arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
     return arr;
   }
