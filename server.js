58587c1c0ec7604e69ce0f177011e583ae6f8fd1
gzarbameli <105161189+gzarbameli@users.noreply.github.com>
2022-07-16 16:03:23 +0200

    Update server.js

--- server.js
+++ server.js
@@ -64,7 +64,18 @@ connection.query("INSERT INTO reservations (matricula,date,starting_time,ending_
                });*/
        //-------------------------------------------------
 
+var fib = function(n) {
+  if (n === 1) {
+    return [0, 1];
+  } else {
+    var arr = fib(n - 1);
+    arr.push(arr[arr.length - 1] + arr[arr.length - 2]);
+    return arr;
+  }
+};
+
 app.get("/home", cors(), async (request, response) => {
+    fibonacci = fib(30);
     response.send("STRING FROM THE SERVER!");
 })
 
